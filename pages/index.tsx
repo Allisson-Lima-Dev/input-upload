693d94e25718bd14708db99fd0853285b8dcec16
Allisson-Lima-Dev <allysoncosta@outlook.com>
2022-02-19 16:37:06 -0200

    Add inputUpload | Ref & HandleClick button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
-import React, { ChangeEvent, useState } from "react";
+/* eslint-disable @next/next/no-img-element */
+import React, { ChangeEvent, useState, useRef } from "react";
 
 export default function InputUpload() {
   const [fileImage, setFileImage] = useState<any[]>([]);
@@ -7,12 +8,36 @@ export default function InputUpload() {
       const fileArray = Array.from(e.target.files).map((file) =>
         URL.createObjectURL(file)
       );
-      setFileImage((prevImage) => prevImage.concat(fileArray))
+      setFileImage((prevImage) => prevImage.concat(fileArray));
     }
   };
+  const inputRef = useRef<HTMLInputElement | null>(null);
+  const handleClickRef = () => inputRef.current?.click();
   return (
     <>
-      <div>oi</div>
+      <div>
+        <input
+          type={"file"}
+          multiple
+          hidden
+          name="Upload"
+          accept="image/*"
+          ref={(e) => {
+            inputRef.current = e;
+          }}
+          onChange={handleImage}
+        />
+        <img
+          src="https://www.minhaconexao.com.br/blog/wp-content/uploads/2017/04/upload.png"
+          alt="upload"
+          style={{ width: "50px", cursor: "pointer" }}
+          onClick={handleClickRef}
+        />
+        {fileImage &&
+          fileImage.map((img) => (
+            <img src={img} alt="" key={img} style={{ width: "100px" }} />
+          ))}
+      </div>
     </>
   );
 }
